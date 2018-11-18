f65ce426868003616d4bac1e841aaa0419009cba
Vaida Pakulyte <vaidapakulyte@mod.lan>
2018-11-19 00:16:12 +0100

    cubejs amend app js file

--- src/App.js
+++ src/App.js
@@ -1,28 +1,31 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { LineChart, Line, XAxis, YAxis } from 'recharts';
+import cubejs from '@cubejs-client/core';
+import { QueryRenderer } from '@cubejs-client/react';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+const cubejsApi = cubejs('eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpIjo0MDIwOX0.RJCWIKz9CeLg8DE94JFpbkZVxbCdenKhta28LnTwhtE');
+
+export default () => {
+  return (
+    <QueryRenderer 
+      query={{
+        measures: ['integration_log.count'],
+        dimensions: ['integration_log.date_created']
+      }} 
+      cubejsApi={cubejsApi} 
+      render={({ resultSet }) => {
+        if (!resultSet) {
+          return 'Loading...';
+        }
+
+        return (
+          <LineChart data={resultSet.rawData()}>
+            <XAxis dataKey="integration_log.date_created"/>
+            <YAxis/>
+            <Line type="monotone" dataKey="integration_log..count" stroke="#8884d8"/>
+          </LineChart>
+        );
+      }}
+    />
+  )
 }
-
-export default App;
